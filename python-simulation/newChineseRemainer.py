e1ff4600398ebdb186daa5ae94c0335c5f464191
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-07-22 09:55:31 +0200

    PEP8

--- python-simulation/newChineseRemainer.py
+++ python-simulation/newChineseRemainer.py
@@ -1,23 +1,23 @@
 import myAlgebra
 import algorithms
 
-def chineseRemainder(a,n):
+
+def chineseRemainder(a, n):
 	'''
 	a is a list of lists, each sublist containing all possible a_i for the same n
 	n is a list of the n_i values
 	'''
-	sumChunks = []
-	
 	H = myAlgebra.lcmArray(n)
-	
+
 # 	calculate all terms of the sums
+	sumChunks = []
 	for i in range(len(n)):
 		sumChunks.append([])
 		Mi = H / n[i]
 		invMi = myAlgebra.modinv(Mi, n[i])
 		for aValue in a[i]:
 			sumChunks[i].append(aValue * Mi * invMi)
-			
+
 # 	calculate the sums from the precomputed terms
 	results = [0]
 	for i in range(len(n)):
@@ -26,17 +26,17 @@ def chineseRemainder(a,n):
 			for r in results:
 				newResults.append(r + sumChunks[i][j])
 		results = newResults
-			
-	return [r % H for r in results]		
 
-print chineseRemainder([[1,2],[4],[3,6]],[3,5,7])
-print myAlgebra.chineseRemainderTheorem([1,4,3],[3,5,7])
-print myAlgebra.chineseRemainderTheorem([1,4,6],[3,5,7])
-print myAlgebra.chineseRemainderTheorem([2,4,3],[3,5,7])
-print myAlgebra.chineseRemainderTheorem([2,4,6],[3,5,7])
-assert chineseRemainder([[1],[4],[3]],[3,5,7])[0] == myAlgebra.chineseRemainderTheorem([1,4,3],[3,5,7])
+	return [r % H for r in results]
 
-ps = myAlgebra.toPrimalPowerSystem([3,5,7])
-print myAlgebra.congruencePrimalPower(ps, [1,4,3])
+print chineseRemainder([[1, 2], [4], [3, 6]], [3, 5, 7])
+print myAlgebra.chineseRemainderTheorem([1, 4, 3], [3, 5, 7])
+print myAlgebra.chineseRemainderTheorem([1, 4, 6], [3, 5, 7])
+print myAlgebra.chineseRemainderTheorem([2, 4, 3], [3, 5, 7])
+print myAlgebra.chineseRemainderTheorem([2, 4, 6], [3, 5, 7])
+assert chineseRemainder([[1], [4], [3]], [3, 5, 7])[0] == myAlgebra.chineseRemainderTheorem([1, 4, 3], [3, 5, 7])
+
+ps = myAlgebra.toPrimalPowerSystem([3, 5, 7])
+print myAlgebra.congruencePrimalPower(ps, [1, 4, 3])
 
 #TODO convert primalsystem to usable stuff
