d4b78857f7f3cc9f3643eb0c32bcb2ae9b57281c
Paul <kafein1418@gmail.com>
2013-07-30 15:46:20 +0200

    Cspace class

--- python-simulation/cspace.py
+++ python-simulation/cspace.py
@@ -9,64 +9,111 @@ import math
 import os.path
 import re
 
-def Cspace(tau, upperLimit="def", lowerLimit = 0):
-	# return a system of inequations of the form
-	# cst_1 * C_1 + cst_2 * C_2 + ... + cst_n * C_n <= CST
-	# encoded as a list
-	# [cst_1, cst_2, ..., cst_n, CST]
+class Cspace(object):
+	def __init__(self, tau, upperLimit='def', lowerLimit=0, fromList=False):
+# 	 return a system of inequations of the form
+		# cst_1 * C_1 + cst_2 * C_2 + ... + cst_n * C_n <= CST
+		# encoded as a list
+		# [cst_1, cst_2, ..., cst_n, CST]
+		
+		if fromList:
+			self.equations = tau
+		else:		
+			Omax = tau.omax()
+			isSynchronous = (Omax == 0)
+		
+			if upperLimit == "def":
+				firstDIT = algorithms.findFirstDIT(tau)
+				if isSynchronous:
+					upperLimit = firstDIT
+					lowerLimit = 0
+				else:
+					if firstDIT is not None:
+						upperLimit = firstDIT + tau.hyperPeriod()
+						lowerLimit = firstDIT
+					else:
+						upperLimit = Omax + 2 * tau.hyperPeriod()
+						lowerLimit = Omax
+		
+			# for each arrival and each deadline, create an equation
+			self.equations = []
+			for a, d in tau.dbf_intervals(lowerLimit, upperLimit):
+				self.equations.append([algorithms.completedJobCount(t, a, d) for t in tau.tasks] + [d - a])
+		
+			
+	def __getitem__(self,key):
+		return self.equations.__getitem__(key)
 
-	Omax = max([task.O for task in tau.tasks])
-	isSynchronous = (Omax == 0)
 
-	if upperLimit == "def":
-		firstDIT = algorithms.findFirstDIT(tau)
-		if isSynchronous:
-			upperLimit = firstDIT
-			lowerLimit = 0
+	def __setitem__(self,key,val):
+		return self.equations.__getitem__(key,val)
+
+
+	def __delitem__(self,key):
+		return self.equations.__getitem__(key)
+	
+	
+	def __len__(self):
+		return self.equations.__len__()
+
+# def Cspace(tau, upperLimit="def", lowerLimit = 0):
+# 	# return a system of inequations of the form
+# 	# cst_1 * C_1 + cst_2 * C_2 + ... + cst_n * C_n <= CST
+# 	# encoded as a list
+# 	# [cst_1, cst_2, ..., cst_n, CST]
+# 
+# 	Omax = max([task.O for task in tau.tasks])
+# 	isSynchronous = (Omax == 0)
+# 
+# 	if upperLimit == "def":
+# 		firstDIT = algorithms.findFirstDIT(tau)
+# 		if isSynchronous:
+# 			upperLimit = firstDIT
+# 			lowerLimit = 0
+# 		else:
+# 			if firstDIT is not None:
+# 				upperLimit = firstDIT + tau.hyperPeriod()
+# 				lowerLimit = firstDIT
+# 			else:
+# 				upperLimit = Omax + 2 * tau.hyperPeriod()
+# 				lowerLimit = Omax
+# 
+# 	# for each arrival and each deadline, create an equation
+# 	equations = []
+# 	for a, d in tau.dbf_intervals(lowerLimit, upperLimit):
+# 		equations.append([algorithms.completedJobCount(t, a, d) for t in tau.tasks] + [d - a])
+# 
+# 	return equations
+
+
+	def removeRedundancy(self, firstPass=True, verbose=False):
+		# Idea:
+		# start with an empty list of cstr,
+		# add each cstr only if it is not redundant
+		# THEN test every remaining cstr against the others
+	
+		# first pass - filter against previous cstr
+		newCspace = []
+		if firstPass:
+			for i, cstr in enumerate(self):
+				if not isRedundant(cstr, newCspace):
+					if verbose: print "\tNon-redundant at step ", i, ":", cstr
+					newCspace.append(cstr)
 		else:
-			if firstDIT is not None:
-				upperLimit = firstDIT + tau.hyperPeriod()
-				lowerLimit = firstDIT
-			else:
-				upperLimit = Omax + 2 * tau.hyperPeriod()
-				lowerLimit = Omax
-
-	# for each arrival and each deadline, create an equation
-	equations = []
-	for a, d in tau.dbf_intervals(lowerLimit, upperLimit):
-		equations.append([algorithms.completedJobCount(t, a, d) for t in tau.tasks] + [d - a])
-
-	return equations
-
-
-def removeRedundancy(cspace, firstPass=True, verbose=False):
-	# Idea:
-	# start with an empty list of cstr,
-	# add each cstr only if it is not redundant
-	# THEN test every remaining cstr against the others
-
-	# first pass - filter against previous cstr
-	newCspace = []
-	if firstPass:
-		for i, cstr in enumerate(cspace):
+			newCspace.extend(cspace)
+	
+		# second pass - filter against all cstr
+		# Note that the number of cstr should be small
+		i = 0
+		while i < len(newCspace):
+			cstr = newCspace.pop(i)
 			if not isRedundant(cstr, newCspace):
-				if verbose: print "\tNon-redundant at step ", i, ":", cstr
-				newCspace.append(cstr)
-	else:
-		newCspace.extend(cspace)
-
-	# second pass - filter against all cstr
-	# Note that the number of cstr should be small
-	i = 0
-	while i < len(newCspace):
-		cstr = newCspace.pop(i)
-		if not isRedundant(cstr, newCspace):
-			newCspace.insert(i, cstr)
-			i += 1
-		elif verbose:
-			print "\t", cstr, "was redundant against", newCspace
-
-	return newCspace
+				newCspace.insert(i, cstr)
+				i += 1
+			elif verbose:
+				print "\t", cstr, "was redundant against", newCspace
+	
+		return Cspace(newCspace,fromList=True)
 
 
 def isRedundant(cstr, cspace):
@@ -177,6 +224,7 @@ class CSpaceConstraint(object):  # TODO : make the code use this lovely class
 		self.coeffs = coeffs
 		self.t = t
 
+
 	def __repr__(self):
 		reprStr = ""
 		for i, a in enumerate(self.coeffs):
@@ -184,10 +232,25 @@ class CSpaceConstraint(object):  # TODO : make the code use this lovely class
 		reprStr = reprStr[:-2]  # remove final "+"
 		reprStr += "<=\t" + str(self.t)
 		return reprStr
+	
+	
+	def __getitem__(self,key):
+		return self.coeffs.__getitem__(key)
+
+
+	def __setitem__(self,key,val):
+		return self.coeffs.__getitem__(key,val)
+
+
+	def __delitem__(self,key):
+		return self.coeffs.__getitem__(key)
+	
+	
+	def __len__(self):
+		return self.coeffs.__len__()
 
 
 if __name__ == '__main__':
-
 	tasks = []
 	#                      0, C, D, T
 	tasks.append(Task.Task(0, 1, 5, 7))
@@ -196,49 +259,49 @@ if __name__ == '__main__':
 	tau = Task.TaskSystem(tasks)
 	print tau
 	tau_Cspace = Cspace(tau)
-	tau_Cspace_noredun = removeRedundancy(tau_Cspace)
-
+	tau_Cspace_noredun = tau_Cspace.removeRedundancy()
+	
 	print "FINAL CSPACE"
 	for cstr in tau_Cspace_noredun:
 		print CSpaceConstraint(cstr[:-1], cstr[-1])
-	print len(tau_Cspace), "=>", len(tau_Cspace_noredun), "equations left"
-
-	# tasks = []
-	# #                      0, C, D, T
-	# tasks.append(Task.Task(5, 1, 1, 3))
-	# tasks.append(Task.Task(0, 4, 4, 8))
-	# tau = Task.TaskSystem(tasks)
-	# tau_Cspace = Cspace(tau)
-	# assert testCVector(tau_Cspace, [task.C for task in tau.tasks]) is False
-
-	# # "TEST2"
-
-	# tasks = []
-	# tasks.append(Task.Task(0, 1, 73, 154))
-	# tasks.append(Task.Task(0, 1, 381, 825))
-	# tasks.append(Task.Task(0, 1, 381, 400))
-	# tau = Task.TaskSystem(tasks)
-	# tau_Cspace = Cspace(tau, algorithms.findFirstDIT(tau))
-	# assert testCVector(tau_Cspace, [task.C for task in tau.tasks]) is True
-	# tau_Cspace_noredun = removeRedundancy(tau_Cspace)
-	# assert len(tau_Cspace) > len(tau_Cspace_noredun) == 2, str(tau_Cspace_noredun)
-
-	# # RANDOM TEST
-	# NUMBER_OF_SYSTEMS = 10
-	# systemArray = generateSystemArray(NUMBER_OF_SYSTEMS, 1)
-	# for tau in systemArray:
-	# 	print tau
-	# 	print "cspace..."
-	# 	cspace = Cspace(tau)
-	# 	print "found ", len(cspace), "equations"
-	# 	print "remove redun..."
-	# 	cspace_noredun = removeRedundancy(cspace)
-	# 	print len(cspace), "=>", len(cspace_noredun), "equations left"
-	# 	print ""
-	# 	resultCSPACE = testCVector(cspace_noredun, [task.C for task in tau.tasks])
-	# 	resultCSPACENOREDUN = testCVector(cspace, [task.C for task in tau.tasks])
-	# 	resultDBF = algorithms.dbfTest(tau)
-	# 	assert resultCSPACE == resultCSPACENOREDUN == resultDBF, str(resultCSPACE) + str(resultCSPACENOREDUN) + str(resultDBF)
-	# 	print "redundancy (necessary) condition ok"
-	# 	print "synchronous instant", algorithms.findSynchronousInstant(tau)
-	# 	print "cspacesize", CspaceSize(tau, cspace_noredun)
+		print len(tau_Cspace), "=>", len(tau_Cspace_noredun), "equations left"
+	
+	tasks = []
+	#                      0, C, D, T
+	tasks.append(Task.Task(5, 1, 1, 3))
+	tasks.append(Task.Task(0, 4, 4, 8))
+	tau = Task.TaskSystem(tasks)
+	tau_Cspace = Cspace(tau)
+	assert testCVector(tau_Cspace, [task.C for task in tau.tasks]) is False
+	
+	# "TEST2"
+	
+	tasks = []
+	tasks.append(Task.Task(0, 1, 73, 154))
+	tasks.append(Task.Task(0, 1, 381, 825))
+	tasks.append(Task.Task(0, 1, 381, 400))
+	tau = Task.TaskSystem(tasks)
+	tau_Cspace = Cspace(tau, algorithms.findFirstDIT(tau))
+	assert testCVector(tau_Cspace, [task.C for task in tau.tasks]) is True
+	tau_Cspace_noredun = tau_Cspace.removeRedundancy()
+	assert len(tau_Cspace) > len(tau_Cspace_noredun) == 2, str(tau_Cspace_noredun)
+	
+	# RANDOM TEST
+	NUMBER_OF_SYSTEMS = 10
+	systemArray = generateSystemArray(NUMBER_OF_SYSTEMS, 1)
+	for tau in systemArray:
+		print tau
+		print "cspace..."
+		cspace = Cspace(tau)
+		print "found ", len(cspace), "equations"
+		print "remove redun..."
+		cspace_noredun = cspace.removeRedundancy()
+		print len(cspace), "=>", len(cspace_noredun), "equations left"
+		print ""
+		resultCSPACE = testCVector(cspace_noredun, [task.C for task in tau.tasks])
+		resultCSPACENOREDUN = testCVector(cspace, [task.C for task in tau.tasks])
+		resultDBF = algorithms.dbfTest(tau)
+		assert resultCSPACE == resultCSPACENOREDUN == resultDBF, str(resultCSPACE) + str(resultCSPACENOREDUN) + str(resultDBF)
+		print "redundancy (necessary) condition ok"
+		print "synchronous instant", algorithms.findSynchronousInstant(tau)
+		print "cspacesize", CspaceSize(tau, cspace_noredun)
