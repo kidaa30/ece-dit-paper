96c7e7f85ffdc7a049d1f11ae839c7ac3e5b8e8f
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-08-07 17:05:49 +0200

    Added (my own) SpotlightEDF and ChooseKeepEDF Two DP algorithms which shows good results (Spotlight is not optimal but maybe CKEDF is???)

--- python-simulation/Simulator/Scheduler.py
+++ python-simulation/Simulator/Scheduler.py
@@ -3,18 +3,88 @@ import Simulator
 import itertools
 
 
-class SchedulerFJP(object):
+class SchedulerDP(object):
+	def __init__(self, tau):
+		self.simu = None
+		pass
+
+	def priority(self, job, simu):
+		pass
+
+
+class ChooseKeepEDF(SchedulerDP):
+	def __init__(self, tau):
+		self.spotlight = SpotlightEDF(tau)
+		self.idlePriority = None
+
+	def idleCPUsCount(self, simu):
+		return len(filter(lambda cpu: cpu.job is None, simu.CPUs))
+
+	def earliestPreempArrival(self, job, simu):
+		raise NotImplementedError, "This is wrinkling my brain"
+
+	def finishTime(self, job, simu):
+		finish = simu.t
+		finish += job.task.C - job.computation
+		if job.preempted:
+			finish += simu.alpha
+		return finish
+
+	def priority(self, job, simu):
+		if self.idleCPUsCount(simu) > 0:
+			epa = {}  # earliest preemptive arrival
+			finishTime = {}
+			# compute epa and finishTime
+			waitingJobs = simu.allCurentJobs(busyJobs=False)
+			if job not in waitingJobs:
+				# busy job (while there are idle CPU), maximal priority
+				return float('inf')
+			for job in waitingJobs:
+				epa[job] = self.earliestPreempArrival(job)
+				finishTime[job] = self.finishTime(job)
+			# if there are easy jobs (jobs which can be computed entirely without being
+			# preempted), there is surely no need to have idle times
+			easyJobs = filter(lambda job: finishtime[job] <= epa[job], waitingJobs)
+			if len(easyJobs) > 0:
+				self.idlePriority = None
+				if job in easyJobs:
+					return 1.0/(job.deadline - simu.t)
+				else:
+					return 1.0/(job.deadline - simu.t + simu.alpha)  # I'm not sure this works
+			else:
+				self.idlePriority = 0
+				return epa[job] - simu.alpha
+		else:
+			return self.spotlight.priority(job, simu)
+
+
+class SpotlightEDF(SchedulerDP):
+	def isJobExecuting(self, job, simu):
+		for cpu in simu.CPUs:
+			if cpu.job is job:
+				return True
+		return False
+
+	def priority(self, job, simu):
+		if self.isJobExecuting(job, simu):
+			return 1.0/(job.deadline - simu.alpha)
+		else:
+			return 1.0/job.deadline
+
+
+class SchedulerFJP(SchedulerDP):
 	def __init__(self, tau):
 		pass
 
-	def priority(self, job):
-		pass
+	def priority(self, job, simu):
+		if job.priority is not None:
+			return job.priority
 
-	def maxPriority(self, jobs):
-		return max([self.priority(job) for job in jobs])
 
-	def minPriority(self, jobs):
-		return min([self.priority(job) for job in jobs])
+class EDF(SchedulerFJP):
+	def priority(self, job, simu):
+		super(SchedulerFJP, self).priority(job, simu)
+		return 1.0/job.deadline
 
 
 class SchedulerFTP(SchedulerFJP):
@@ -22,10 +92,11 @@ class SchedulerFTP(SchedulerFJP):
 		self.priorities = self.orderPriorities(tau.tasks)
 
 	def orderPriorities(self, taskArray):
-		# put the self.priorities array in priority order (decreasing)
+		# return priorities array in priority order (decreasing)
 		pass
 
-	def priority(self, job):
+	def priority(self, job, simu):
+		super(SchedulerFJP, self).priority(job, simu)
 		for i, task in enumerate(reversed(self.priorities)):  # priorities is decreasing
 			if job.task is task:
 				return i + 1  # prio = 0 is for idle cpu
@@ -49,9 +120,9 @@ class FixedPriority(SchedulerFTP):
 
 
 class ExhaustiveFixedPriority(FixedPriority):
-	def __init__(self, tau, preemptTime, m, abortAndRestart):
+	def __init__(self, tau, preempTime, m, abortAndRestart):
 		self.tau = tau
-		self.preemptTime = preemptTime
+		self.preempTime = preempTime
 		self.m = m
 		self.abortAndRestart = abortAndRestart
 		self.foundFeasible = None
@@ -63,7 +134,7 @@ class ExhaustiveFixedPriority(FixedPriority):
 		priorities = [i for i in range(0, len(taskArray))]
 		self.foundFeasible = False
 		for prio in itertools.permutations(priorities):
-			simu = Simulator.Simulator(self.tau, None, self.preemptTime, self.m, FixedPriority(self.tau, prio), self.abortAndRestart)
+			simu = Simulator.Simulator(self.tau, None, self.preempTime, self.m, FixedPriority(self.tau, prio), self.abortAndRestart)
 			simu.run(stopAtDeadlineMiss=True, verbose=False)
 			if simu.success():
 				self.foundFeasible = True
@@ -83,6 +154,4 @@ class RM(SchedulerFTP):
 		return priorities
 
 
-class EDF(SchedulerFJP):
-	def priority(self, job):
-		return 1.0/job.deadline
+
