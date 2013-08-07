623dc8e53c6dde5dcc24fd0d7964d6e6bcff8340
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-08-07 17:03:50 +0200

    Moved interesting systems in the systems module

--- python-simulation/mainSimu.py
+++ python-simulation/mainSimu.py
@@ -3,57 +3,14 @@ from Model import TaskGenerator
 from Model import algorithms
 from Simulator import Simulator
 from Simulator import Scheduler
+import systems
 
-import random
-
-def generateSystemArray(numberOfSystems, constrDeadlineFactor, verbose=False):
-	systemArray = []
-	for i in range(numberOfSystems):
-		Umin = 0.75
-		Umax = 0.95
-		Utot = 1.0*random.randint(int(Umin*100), int(Umax*100))/100
-		n = 5
-		maxHyperT = 180  # PPCM(2, 3, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 22, 24, 25, 28, 30, 32)
-		# maxHyperT = -1
-		Tmin = 5
-		Tmax = 50
-		tasks = TaskGenerator.generateTasks(Utot, n, maxHyperT, Tmin, Tmax, synchronous=False, constrDeadlineFactor=constrDeadlineFactor)
-		if (verbose and numberOfSystems <= 10):
-			print "Generated task system # ", i
-			for task in tasks:
-					print "\t", task
-		systemArray.append(Task.TaskSystem(tasks))
-	return systemArray
-
-
-tasks = []
-
-# # exemple from Patrick Meumeu's thesis pp. 128 (Fig. 4.13)
-# tasks.append(Task.Task(0, 3, 7, 15))
-# tasks.append(Task.Task(5, 2, 6, 6))
-# tasks.append(Task.Task(3, 4, 10, 10))
-# tau = Task.TaskSystem(tasks)
-
-# exemple from Wong/Burns paper (Improved Priority Assignment for the Abort-and-Restart (AR) Model)
-tasks.append(Task.Task(0, 6, 60, 60))
-tasks.append(Task.Task(0, 4, 32, 32))
-tasks.append(Task.Task(0, 3, 25, 25))
-tasks.append(Task.Task(0, 5, 50, 50))
-tasks.append(Task.Task(0, 2, 100, 100))
-tau = Task.TaskSystem(tasks)
-
-# tau = generateSystemArray(1, 1)[0]
-
-# exemple of non-optimality of EDF with preemptions
-# tasks.append(Task.Task(0, 3, 6, 6))
-# tasks.append(Task.Task(1, 2, 4, 4))
-# tau = Task.TaskSystem(tasks)
+#tau = systems.generateSystemArray(1, 1)[0]
+tau = systems.SpotlightEDFNonOptimal
 
 Omax = max([task.O for task in tau.tasks])
 H = tau.hyperPeriod()
 fpdit = algorithms.findFirstDIT(tau)
-
-print tau
 
 print "Omax", Omax
 print "H", H
@@ -66,18 +23,19 @@ if fpdit:
 print "stop", stop
 
 # scheduler = Scheduler.EDF(tau)
-# scheduler = Scheduler.FixedPriority(tau, [2, 1])
+scheduler = Scheduler.SpotlightEDF(tau)
+# scheduler = Scheduler.FixedPriority(tau, [3, 1, 2])
 # !!! exhaustive: set the parameters right !!!
-scheduler = Scheduler.ExhaustiveFixedPriority(tau, 0, 1, True)
-if scheduler.foundFeasible:
-	print "found feasible priorities :", scheduler.prioArray
-else:
-	print "No feasible priorities found ! This will end badly"
+# scheduler = Scheduler.ExhaustiveFixedPriority(tau, preempTime=2, m=1, abortAndRestart=False)
+# if scheduler.foundFeasible:
+# 	print "found feasible priorities :", scheduler.prioArray
+# else:
+# 	print "No feasible priorities found ! This will end badly"
 
-simu = Simulator.Simulator(tau, stop=stop, preempTime=0, m=1, scheduler=scheduler, abortAndRestart=True)
+simu = Simulator.Simulator(tau, stop=stop, preempTime=2, m=1, scheduler=scheduler, abortAndRestart=False)
 
 try:
-	simu.run(stopAtDeadlineMiss=True, verbose=False)
+	simu.run(stopAtDeadlineMiss=True, verbose=True)
 	if simu.success():
 		print "No deadline misses."
 	else:
---
+++ python-simulation/systems.py
@@ -0,0 +1,65 @@
+import random
+from Model import Task
+from Model import TaskGenerator
+
+def generateSystemArray(numberOfSystems, constrDeadlineFactor, verbose=False):
+	systemArray = []
+	for i in range(numberOfSystems):
+		Umin = 0.75
+		Umax = 0.95
+		Utot = 1.0*random.randint(int(Umin*100), int(Umax*100))/100
+		n = 5
+		maxHyperT = 180  # PPCM(2, 3, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 22, 24, 25, 28, 30, 32)
+		# maxHyperT = -1
+		Tmin = 5
+		Tmax = 50
+		tasks = TaskGenerator.generateTasks(Utot, n, maxHyperT, Tmin, Tmax, synchronous=False, constrDeadlineFactor=constrDeadlineFactor)
+		if (verbose and numberOfSystems <= 10):
+			print "Generated task system # ", i
+			for task in tasks:
+					print "\t", task
+		systemArray.append(Task.TaskSystem(tasks))
+	return systemArray
+
+
+# exemple from Patrick Meumeu's thesis pp. 128 (Fig. 4.13)
+tasks = []
+tasks.append(Task.Task(0, 3, 7, 15))
+tasks.append(Task.Task(5, 2, 6, 6))
+tasks.append(Task.Task(3, 4, 10, 10))
+Meumeu = Task.TaskSystem(tasks)
+
+# exemple from Wong/Burns paper (Improved Priority Assignment for the Abort-and-Restart (AR) Model)
+tasks = []
+tasks.append(Task.Task(0, 6, 60, 60))
+tasks.append(Task.Task(0, 4, 32, 32))
+tasks.append(Task.Task(0, 3, 25, 25))
+tasks.append(Task.Task(0, 5, 50, 50))
+tasks.append(Task.Task(0, 2, 100, 100))
+WongBurns = Task.TaskSystem(tasks)
+
+# exemple of non-optimality of EDF with preemptions
+tasks = []
+tasks.append(Task.Task(0, 3, 6, 6))
+tasks.append(Task.Task(1, 2, 4, 4))
+EDFNonOptimal = Task.TaskSystem(tasks)
+
+# Anomaly: removing Task (0,1,2,4) renders the system unfeasible!
+tasks = []
+tasks.append(Task.Task(0, 2, 4, 4))
+tasks.append(Task.Task(1, 1, 1, 4))
+Anomaly1 = Task.TaskSystem(tasks)
+
+# # Example of non-optimality of SpotlightEDF
+tasks = []
+tasks.append(Task.Task(0, 3, 6, 7))
+tasks.append(Task.Task(0, 2, 7, 7))
+tasks.append(Task.Task(2, 1, 1, 7))
+SpotlightEDFNonOptimal = Task.TaskSystem(tasks)
+
+# # PreemptNoIdle
+tasks = []
+tasks.append(Task.Task(0, 6, 9, 11))
+tasks.append(Task.Task(0, 2, 11, 11))
+tasks.append(Task.Task(3, 2, 2, 11))
+PreemptNoIdle = Task.TaskSystem(tasks)
