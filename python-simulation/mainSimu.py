c12089f293ccb6edf7a84ac8d99bc8e289a95045
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-08-27 11:58:08 +0200

    Changed indentation to spaces + some changes

--- python-simulation/mainSimu.py
+++ python-simulation/mainSimu.py
@@ -3,8 +3,10 @@ from Simulator import Simulator
 from Simulator import Scheduler
 import systems
 
+import subprocess
+
 # tau = systems.generateSystemArray(1, 1)[0]
-tau = systems.DPOnly2
+tau = systems.LongTransitive
 
 Omax = max([task.O for task in tau.tasks])
 H = tau.hyperPeriod()
@@ -16,32 +18,33 @@ print "fpdit", fpdit
 
 stop = Omax + 2 * H
 if fpdit:
-	stop = fpdit + H
+    stop = fpdit + H
 
 print "stop", stop
 
-# scheduler = Scheduler.EDF(tau)
+scheduler = Scheduler.EDF(tau)
 # scheduler = Scheduler.SpotlightEDF(tau)
-scheduler = Scheduler.ChooseKeepEDF(tau, prioOffset=10)
-# scheduler = Scheduler.FixedPriority(tau, [3, 1, 2])
+# scheduler = Scheduler.ChooseKeepEDF(tau, prioOffset=10)
+# scheduler = Scheduler.FixedPriority(tau, [1, 2, 3])
 # !!! exhaustive: set the parameters right !!!
 # scheduler = Scheduler.ExhaustiveFixedPriority(tau, preempTime=2, m=1, abortAndRestart=False)
 # if scheduler.foundFeasible:
-# 	print "found feasible priorities :", scheduler.prioArray
+#   print "found feasible priorities :", scheduler.prioArray
 # else:
-# 	print "No feasible priorities found ! This will end badly"
+#   print "No feasible priorities found ! This will end badly"
 
-simu = Simulator.Simulator(tau, stop=stop, preempTime=1, m=1, scheduler=scheduler, abortAndRestart=False)
+simu = Simulator.Simulator(tau, stop=3450, m=1, scheduler=scheduler, abortAndRestart=False)
 
 try:
-	simu.run(stopAtDeadlineMiss=True, verbose=True)
-	if simu.success():
-		print "No deadline misses."
-	else:
-		print "Deadline miss at t=", simu.t
+    simu.run(stopAtDeadlineMiss=True, verbose=False)
+    if simu.success():
+        print "No deadline misses."
+    else:
+        print "Deadline miss at t=", simu.t
 except AssertionError:
-	print "Something went wrong ! Close the image preview to see the callback"
-	simu.drawer.outImg.show()
-	raise
+    print "Something went wrong ! Close the image preview to see the callback"
+    simu.drawer.outImg.show()
+    raise
 
-simu.drawer.outImg.show()
+simu.drawer.outImg.save("out.png")
+subprocess.Popen(['shotwell', 'out.png'])
