55e4f05aa61718c28b4574154f5e31092a08e1bf
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-08-29 16:26:15 +0200

    Changed simulator behavior
    Separated updatePriorities from updateHeap
    Moved verbose to class level (was method-level)
    Job can be preempted as soon as its preemption recovery period is over

--- python-simulation/Simulator/Simulator.py
+++ python-simulation/Simulator/Simulator.py
@@ -12,8 +12,9 @@ def heappeek(heap):
 
 
 class Simulator(object):  # Global FJP only
-    def __init__(self, tau, stop, nbrCPUs, scheduler, abortAndRestart):
+    def __init__(self, tau, stop, nbrCPUs, scheduler, abortAndRestart, verbose=False):
         """stop can be set to None for default value"""
+        self.verbose = verbose
         self.system = tau
         self.m = nbrCPUs
         self.AR = abortAndRestart
@@ -63,8 +64,19 @@ class Simulator(object):  # Global FJP only
             busyJobs = filter(None, [cpu.job for cpu in self.CPUs])
         return waitingJobs + busyJobs
 
+    def updatePriorities(self, job="all"):
+        jobs = []
+        if job == "all":
+            jobs.extend(self.getCurrentJobs())
+        else:
+            jobs.append(job)
+        for job in jobs:
+            job.priority = self.scheduler.priority(job, self)
+            if self.verbose:
+                print "\t\tpriority of ", job, "is now", job.priority
+
     def updateHeaps(self):
-        # possible bottleneck : job heap is reconstructed (2 times) at each t
+        # possible bottleneck
         newJobHeap = []
         for prio, job in self.activeJobsHeap:
             newJobHeap.append((-1 * job.priority, job))
@@ -78,15 +90,19 @@ class Simulator(object):  # Global FJP only
     def lessPrioritaryCPU(self):
         return heappeek(self.activeCPUsHeap)
 
-    def incrementTime(self, verbose=True):
+    def incrementTime(self):
         self.t += 1
-        if verbose: print "t=", self.t
+        if self.verbose: print "t=", self.t
         # Initialize scheduler
         self.scheduler.initInstant()
         # remove finished job from CPUs
+        self.activateCPUs()
         for cpu in self.activeCPUsHeap:
             if cpu.job and cpu.job.isFinished():
+                if self.verbose:
+                    print "\tCPU ", cpu, "is finished"
                 cpu.job = None
+        self.updatePriorities()
         self.updateHeaps()
         # check for deadline miss
         for job in self.getCurrentJobs():
@@ -98,36 +114,33 @@ class Simulator(object):  # Global FJP only
         for task in self.system.tasks:
             if self.t >= task.O and self.t % task.T == task.O % task.T:
                 newJob = Job(task, self.t)
-                if verbose: print "\tarrival of job:", newJob
                 newJob.priority = self.scheduler.priority(newJob, self)
-                if verbose: print "\t\tprio of ", newJob, "is", newJob.priority
+                if self.verbose: print "\tarrival of job", newJob
                 heappush(self.activeJobsHeap, (-1 * newJob.priority, newJob))
 
         # preemptions
         while True:
-            # update priorities (DP)
-            # if self.t == 113:
-            #     pdb.set_trace()
-            for job in self.getCurrentJobs():
-                job.priority = self.scheduler.priority(job, self)
-                if verbose:
-                    print "\t\tprio of ", job, "is now", job.priority
+            # update priorities
+            self.updatePriorities()
             self.updateHeaps()
-
             # check for preemptions
-            if verbose:
+            if self.verbose:
                 print "\t", self.mostPrioritaryJob(), "(", str(self.mostPrioritaryJob().priority if self.mostPrioritaryJob() else None), ") vs.", self.lessPrioritaryCPU(), "(", str(self.lessPrioritaryCPU().priority() if self.lessPrioritaryCPU() else None), ")"
             if self.mostPrioritaryJob() and self.lessPrioritaryCPU() and self.mostPrioritaryJob().priority >= self.lessPrioritaryCPU().priority():
                 # special case of equal priorities : decided by the scheduler
-                if self.mostPrioritaryJob().priority == self.lessPrioritaryCPU().priority() and not self.scheduler.preemptEqualPriorities():
-                    break
-                if verbose:
+                if self.mostPrioritaryJob().priority == self.lessPrioritaryCPU().priority():
+                    if self.verbose:
+                        print "equal priority: preemption policy of scheduler :", self.scheduler.preemptEqualPriorities()
+                    if not self.scheduler.preemptEqualPriorities():
+                        break
+                if self.verbose:
                     print "\tpremption!"
                 preemptiveJob = heappop(self.activeJobsHeap)[1]
                 preemptedCPU = heappop(self.activeCPUsHeap)
                 preemptedJob = preemptedCPU.job  # may be None
 
                 # assign preemptive job to the CPU and push CPU in the correct heap
+                self.updatePriorities(job=preemptiveJob)
                 preemptedCPU.job = preemptiveJob
                 if preemptiveJob.preempted:
                     preemptiveJob.preempted = False
@@ -144,7 +157,7 @@ class Simulator(object):  # Global FJP only
                         self.drawer.drawAbort(preemptedJob.task, self.t)
                     heappush(self.activeJobsHeap, (-1 * preemptedJob.priority, preemptedJob))
 
-                if verbose:
+                if self.verbose:
                     print "\t", self.mostPrioritaryJob(), "(", str(self.mostPrioritaryJob().priority if self.mostPrioritaryJob() else None), ") vs.", self.lessPrioritaryCPU(), "(", str(self.lessPrioritaryCPU().priority() if self.lessPrioritaryCPU() else None), ")"
             else:
                 break
@@ -157,18 +170,18 @@ class Simulator(object):  # Global FJP only
         # compute preemptions
         for cpu in self.preemptedCPUs:
             cpu.preemptionTimeLeft -= 1
-        if verbose:
+        if self.verbose:
             for i, cpu in enumerate(self.CPUs):
                 print "\t", i, cpu
                 if cpu in self.preemptedCPUs:
                     print "\t(preempt)", cpu.preemptionTimeLeft, "left"
 
-    def run(self, stopAtDeadlineMiss=True, verbose=False):
+    def run(self, stopAtDeadlineMiss=True):
         while(self.t < self.stop):
-            self.incrementTime(verbose=verbose)
+            self.incrementTime()
 
             for miss in self.deadlineMisses:
-                if verbose: print "DEADLINE MISS at t=", (miss[0] - 1), "for job", miss[1]
+                if self.verbose: print "DEADLINE MISS at t=", (miss[0] - 1), "for job", miss[1]
             if len(self.deadlineMisses) > 0:
                 if stopAtDeadlineMiss:
                     break
