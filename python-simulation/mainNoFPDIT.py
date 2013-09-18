3639e80997180f415dcbba8d8482dbf4a8d71a7b
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-18 09:59:20 +0200

    closed issue #29

--- python-simulation/mainNoFPDIT.py
+++ python-simulation/mainNoFPDIT.py
@@ -28,35 +28,28 @@ def generateSystemArray(numberOfSystems, constrDeadlineFactor, tasksCnt, verbose
     return systemArray
 
 if __name__ == '__main__':
-    NUMBER_OF_SYSTEMS = 10
-    noFPDITpcts2 = []
-    noFPDITpcts3 = []
-    noFPDITpcts4 = []
-    CDFvalues = []
-    for constrDeadlineFactor in [1/(x * 0.1) for x in range(1, 11)]:
-        print(("CDF", constrDeadlineFactor))
-        systemArray2 = generateSystemArray(NUMBER_OF_SYSTEMS, constrDeadlineFactor, 2)
-        systemArray3 = generateSystemArray(NUMBER_OF_SYSTEMS, constrDeadlineFactor, 3)
-        systemArray4 = generateSystemArray(NUMBER_OF_SYSTEMS, constrDeadlineFactor, 4)
-        firstDITs2 = []
-        firstDITs3 = []
-        firstDITs4 = []
-        for i, tau in enumerate(systemArray2):
-            firstDITs2.append(algorithms.findFirstPeriodicDIT(tau))
-        for i, tau in enumerate(systemArray3):
-            firstDITs3.append(algorithms.findFirstPeriodicDIT(tau))
-        for i, tau in enumerate(systemArray4):
-            firstDITs4.append(algorithms.findFirstPeriodicDIT(tau))
-        noFPDITpcts2.append((100.0*len([x for x in firstDITs2 if x is None]))/len(firstDITs2))
-        noFPDITpcts3.append((100.0*len([x for x in firstDITs3 if x is None]))/len(firstDITs3))
-        noFPDITpcts4.append((100.0*len([x for x in firstDITs4 if x is None]))/len(firstDITs4))
-        print(("Percentage of system with no FPDIT:", noFPDITpcts2[-1], noFPDITpcts3[-1], noFPDITpcts4[-1], "%"))
-        CDFvalues.append(constrDeadlineFactor)
+    NUMBER_OF_SYSTEMS = 100
+    noFPDITpcts = {}
+    CDFvalues = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0]
+    nValues = [2, 3, 4, 5, 10]
+    for taskCnt in nValues:
+        print("n", taskCnt)
+        noFPDITpcts[taskCnt] = {}
+        for constrDeadlineFactor in CDFvalues:
+            print("cdf", constrDeadlineFactor)
+            systemArray = generateSystemArray(NUMBER_OF_SYSTEMS, constrDeadlineFactor, taskCnt)
+            firstDitsCnt = 0
+            for i, tau in enumerate(systemArray):
+                if algorithms.findFirstPeriodicDIT(tau) is not None:
+                    firstDitsCnt += 1
+            noFPDITpcts[taskCnt][constrDeadlineFactor] = (100*firstDitsCnt)/NUMBER_OF_SYSTEMS
 
     pylab.figure()
-    pylab.plot([1.0/x for x in CDFvalues], noFPDITpcts4, "b-^", label="4 Tasks")
-    pylab.plot([1.0/x for x in CDFvalues], noFPDITpcts3, "k-o", label="3 Tasks")
-    pylab.plot([1.0/x for x in CDFvalues], noFPDITpcts2, "g-s", label="2 Tasks")
+    for taskCnt in nValues:
+        noFPDITpctsPerCDF = []
+        for cdf in CDFvalues:
+            noFPDITpctsPerCDF.append(noFPDITpcts[taskCnt][cdf])
+        pylab.plot(CDFvalues, noFPDITpctsPerCDF, label=str(taskCnt) + " Tasks")
     pylab.ylabel("%")
     pylab.xlabel("CDF")
     pylab.title("Number of systems with no FPDIT")
