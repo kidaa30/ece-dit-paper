82858f0b47d1ea1d2150edd0d5147cf62286d547
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-27 15:46:40 +0200

    changed indentation to spaces

--- python-simulation/Model/CPU.py
+++ python-simulation/Model/CPU.py
@@ -1,15 +1,15 @@
 class CPU(object):
-	def __init__(self):
-		self.job = None
+    def __init__(self):
+        self.job = None
 
-	def isIdle(self):
-		return self.job is None
+    def isIdle(self):
+        return self.job is None
 
-	def priority(self):
-		return self.job.priority if self.job else 0
+    def priority(self):
+        return self.job.priority if self.job else 0
 
-	def __lt__(self, other):
-		return self.priority() < other.priority()
+    def __lt__(self, other):
+        return self.priority() < other.priority()
 
-	def __str__(self):
-		return "CPU " + str(id(self)) + " containing " + str(self.job)
+    def __str__(self):
+        return "CPU " + str(id(self)) + " containing " + str(self.job)
