ffa5878c5bf9742bfa5bbed3f17120984ed4f5ca
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-28 19:23:16 +0200

    More random tests Removed edge-case test which was not edge case after all

--- python-simulation/test/testAlgorithms.py
+++ python-simulation/test/testAlgorithms.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 
 from Model import algorithms as algo
 from Model.Task import Task, TaskSystem
@@ -80,23 +81,14 @@ class TestAlgorithms(unittest.TestCase):
         self.assertEquals(algo.findSynchronousInstant(tau), 0)
         self.assertFalse(algo.dbfTest(tau))
 
-    def test_YAfindFPDIT(self):
-        tasks = []
-        tasks.append(Task(10, 2, 2, 5))
-        tasks.append(Task(5, 1, 2, 10))
-        tasks.append(Task(5, 1, 4, 5))
-        tasks.append(Task(0, 1, 4, 5))
-        tau = TaskSystem(tasks)
-        self.assertEquals(algo.YAfindFPDIT(tau), algo.findFirstDIT(tau))
-
     def test_randomYAfindFPDIT(self):
         from Model import TaskGenerator
-        Utot = 1
-        n = 4
-        maxHyperT = 100
-        Tmin = 5
-        Tmax = 20
         for i in range(1000):
+            Utot = random.randint(25, 100) / 100
+            n = random.randint(2, 5)
+            maxHyperT = 5046
+            Tmin = 5
+            Tmax = 20
             tasks = TaskGenerator.generateTasks(Utot, n, maxHyperT, Tmin, Tmax, synchronous=False)
             tau = TaskSystem(tasks)
             self.assertEquals(algo.YAfindFPDIT(tau), algo.findFirstDIT(tau))
